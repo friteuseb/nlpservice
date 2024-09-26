4894101879c4b1cef6098d71808f37b054fd3819
friteuseb <cyril.wolfangel@gmail.com>
2024-09-26 14:25:24 +0200

    ajout de swagger docs

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,6 +1,18 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from . import web_bp
+import os
+
+# Chemin vers le répertoire contenant le fichier openapi.yaml
+swagger_dir = os.path.join(os.path.dirname(__file__), 'static', 'swagger')
 
 @web_bp.route('/')
 def home():
     return render_template('index.html')
+
+@web_bp.route('/api-docs')
+def swagger_ui():
+    return render_template('swagger.html')
+
+@web_bp.route('/openapi.yaml')
+def send_openapi_spec():
+    return send_from_directory(swagger_dir, 'openapi.yaml')
