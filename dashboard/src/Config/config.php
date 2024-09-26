76a39ea41e1066f1ea5939210507605ff803b880
cyril.wolfangel@talan.com <cyril.wolfangel@talan.com>
2024-09-26 08:11:18 +0200

    add dashboard project php test

--- dashboard/src/Config/config.php
+++ dashboard/src/Config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('API_URL', 'https://nlpservice.semantic-suggestion.com/api');
+
 define('TEXTS_DIRECTORY', __DIR__ . '/../../texts');
 define('ANALYSES_DIRECTORY', __DIR__ . '/../../data/analyses');
 define('SIMILARITIES_DIRECTORY', __DIR__ . '/../../data/similarities');
@@ -14,4 +14,18 @@ if (!file_exists(ANALYSES_DIRECTORY)) {
 }
 if (!file_exists(SIMILARITIES_DIRECTORY)) {
     mkdir(SIMILARITIES_DIRECTORY, 0755, true);
+}
+
+// Déterminez si vous êtes en environnement local
+function isLocalEnvironment() {
+    $localDomains = ['localhost', '127.0.0.1', 'nlpservice.ddev.site'];
+           strpos($_SERVER['SERVER_NAME'], '.local') !== false ||
+           strpos($_SERVER['SERVER_NAME'], '.test') !== false;
+}
+
+// Définissez l'URL de l'API en fonction de l'environnement
+if (isLocalEnvironment()) {
+    define('API_URL', '127.0.0.1:5000/api');  // Utilisez l'adresse IP locale et le port approprié
+} else {
+    define('API_URL', 'https://nlpservice.semantic-suggestion.com/api');
 }
